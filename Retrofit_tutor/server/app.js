04653363950fa709959e9de08016d6aceaf48670
clucle <wjdenwls123@gmail.com>
2017-05-18 22:53:50 +0900

    feat: update server
    server get retrofit connection, and send Client Model, Model List

--- Retrofit_tutor/server/app.js
+++ Retrofit_tutor/server/app.js
@@ -5,6 +5,13 @@ var path = require('path');
 
 var app = express();
 
+var bodyParser = require('body-parser');
+
+// parse JSON inputs
+app.use(bodyParser.json());
+// parse URL encoded inputs
+app.use(bodyParser.urlencoded());
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res){
@@ -12,12 +19,17 @@ app.get('/', function(req, res){
 });
 
 app.post('/ping', function(req, res){
-	//res.status(404).send({error: 'Error!'});
 	res.status(200).send("pong");
 });
 
 app.post('/user/one', function(req, res){
-	res.status(200).send({name: 'clucle', age: 23});
+	if (req.body.name == "clucle") {
+		res.status(200).send({name: 'clucle', age: 23});
+	} else if(req.body.name == "junsu") {
+		res.status(200).send({name: 'junsu', age: 25});
+	} else {
+		res.status(404).send("ERROR");
+	}
 });
 
 app.post('/user/list', function(req, res){
@@ -26,8 +38,6 @@ app.post('/user/list', function(req, res){
 		{name: 'junsu', age: 25}
 	]});
 });
-
-
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listeneing on port ' + app.get('port'));
