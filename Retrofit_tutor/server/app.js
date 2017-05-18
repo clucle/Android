05d04521b7da2dd77aba9ec3620dfb7978d2b931
clucle <wjdenwls123@gmail.com>
2017-05-18 21:11:27 +0900

    feat: simple HTTP Post Call

--- Retrofit_tutor/server/app.js
+++ Retrofit_tutor/server/app.js
@@ -12,8 +12,22 @@ app.get('/', function(req, res){
 });
 
 app.post('/ping', function(req, res){
-	res.send("pong");
+	//res.status(404).send({error: 'Error!'});
+	res.status(200).send("pong");
 });
+
+app.post('/user/one', function(req, res){
+	res.status(200).send({name: 'clucle', age: 23});
+});
+
+app.post('/user/list', function(req, res){
+	res.status(200).send({users: [
+		{name: 'clucle', age: 23},
+		{name: 'junsu', age: 25}
+	]});
+});
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listeneing on port ' + app.get('port'));
