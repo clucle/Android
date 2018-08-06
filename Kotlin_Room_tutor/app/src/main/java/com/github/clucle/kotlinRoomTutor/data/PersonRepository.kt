fcb51a62dee492e22caa22c2fd864ea2a8985fa5
clucle <wjdenwls123@gmail.com>
2018-08-06 14:48:24 +0900

    feat: create person repository
    Signed-off-by: clucle <wjdenwls123@gmail.com>

---
+++ Kotlin_Room_tutor/app/src/main/java/com/github/clucle/kotlinRoomTutor/data/PersonRepository.kt
@@ -0,0 +1,25 @@
+package com.github.clucle.kotlinRoomTutor.data
+
+import com.github.clucle.kotlinRoomTutor.utilities.runOnIoThread
+
+class PersonRepository private constructor(
+        private val personDao: PersonDao
+) {
+    fun createPerson(firstName: String, lastName: String) {
+        runOnIoThread {
+            val person = Person(0, firstName = firstName, lastName = lastName)
+            personDao.insert(person)
+        }
+    }
+
+    fun getAllPeople() = personDao.getAllPeople()
+
+    companion object {
+        @Volatile private var instance: PersonRepository? = null
+
+        fun getInstance(personDao: PersonDao) =
+                instance ?: synchronized(this) {
+                    instance ?: PersonRepository(personDao).also { instance = it }
+                }
+    }
+}
