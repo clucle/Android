13e0404df717dc3514d609309b2192335ddaf818
clucle <wjdenwls123@gmail.com>
2018-08-08 19:16:29 +0900

    feat: 버튼 클릭까지 바인딩
    Signed-off-by: clucle <wjdenwls123@gmail.com>

--- Kotlin_Room_tutor/app/src/main/java/com/github/clucle/kotlinRoomTutor/PersonListFragment.kt
+++ Kotlin_Room_tutor/app/src/main/java/com/github/clucle/kotlinRoomTutor/PersonListFragment.kt
@@ -4,32 +4,45 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.github.clucle.kotlinRoomTutor.adapters.PersonAdapter
+import com.github.clucle.kotlinRoomTutor.databinding.FragmentPersonListBinding
 import com.github.clucle.kotlinRoomTutor.utilities.InjectorUtils
 import com.github.clucle.kotlinRoomTutor.viewmodels.PersonListViewModel
 
 class PersonListFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = PersonListFragment()
-    }
-
-
     private lateinit var viewModel: PersonListViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_person_list, container, false)
-        val context = context ?: return view
+        val factory = InjectorUtils.providePersonListViewModelFactory(requireActivity())
+        val personViewModel = ViewModelProviders.of(this, factory)
+                .get(PersonListViewModel::class.java)
+        val adapter = PersonAdapter()
+        val binding: FragmentPersonListBinding = DataBindingUtil.inflate<FragmentPersonListBinding>(
+                inflater, R.layout.fragment_person_list, container, false).apply {
+            viewModel = personViewModel
+            setLifecycleOwner(this@PersonListFragment)
+            personList.adapter = adapter
+            createPersonBtn.setOnClickListener { view ->
+                viewModel.createPerson("first", "last")
+            }
+        }
+        subscribeUi(adapter)
+        return binding.root
+    }
 
-        val factory = InjectorUtils.providePersonListViewModelFactory(context)
-        viewModel = ViewModelProviders.of(this, factory).get(PersonListViewModel::class.java)
-
-        // 후에 처리
-        return view
+    private fun subscribeUi(adapter: PersonAdapter) {
+        viewModel.getAllPerson().observe(viewLifecycleOwner, Observer { persons ->
+            if (persons != null) adapter.submitList(persons)
+        })
     }
 }
